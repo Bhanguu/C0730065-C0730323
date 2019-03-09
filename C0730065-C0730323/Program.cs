e472e2374c9e903b3cf9dc67fd172cfb699f5ab8
visualstudio <visualstudio@122-PC11>
2019-03-08 21:34:38 -0500

    Lab 14

--- C0730065-C0730323/Program.cs
+++ C0730065-C0730323/Program.cs
@@ -14,31 +14,31 @@ namespace C0730065_C0730323
     //Ravneet Kaur (C0730323)
     public static void Main(string[] args)
     {
-        DelegateExercises de = new DelegateExercises();
-        de.Method3();
-        Console.ReadLine();
+            DelegateExercises de = new DelegateExercises();
+            de.Method3();
+            Console.ReadLine();
+        }
     }
-}
-delegate int MyDelegate(out int i);
-public class DelegateExercises
-{
-    int Method1(out int i)
+    delegate int MyDelegate(out int i);
+    public class DelegateExercises
     {
-        Console.WriteLine("Method 1");
-        i = 10;
-        return 0;
-    }
-    public void Method3()
-    {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        MyDelegate myDelegate1 = new MyDelegate(Method1);
-        MyDelegate myDelegate2 = myDelegate + myDelegate1;
-        int intValue;
-        myDelegate2(out intValue);
-    }
+        int Method1(out int i)
+        {
 
+            i = 100;
+            Console.WriteLine("Method 1 " + i);
+            return 0;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = null;
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
+        }
 
-}
+    }
 
 }
     
