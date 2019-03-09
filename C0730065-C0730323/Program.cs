86b667acf3b01f69e6b44b7704f12d21ea2d897e
visualstudio <visualstudio@122-PC11>
2019-03-08 21:40:50 -0500

    lab 16 Finally

--- C0730065-C0730323/Program.cs
+++ C0730065-C0730323/Program.cs
@@ -19,9 +19,65 @@ namespace C0730065_C0730323
             delegateExercises.Method3();
 
             Console.ReadLine();
+
         }
+
+
+
+
+
     }
+
 }
+
+
+
+
+
+
+
+public delegate void MyDelegate(ref int intValue);
+
+
+
+public class DelegateExercises
+
+{
+
+
+
+    void Method1(ref int intValue)
+
+    {
+
+
+
+        intValue = intValue + 5;
+
+        System.Console.WriteLine("Method1 " + intValue);
+
+    }
+
+
+
+    public void Method3()
+
+    {
+
+        MyDelegate myDelegate = new MyDelegate(Method1);
+
+        MyDelegate myDelegate1 = new MyDelegate(Method1);
+
+        MyDelegate myDelegate2 = myDelegate + myDelegate1;
+
+        int intParameter = 5;
+
+        myDelegate2(ref intParameter);
+
+    }
+
+}
+
 
 
 
