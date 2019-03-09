276ff3fc656f0168db65eb037574bc3c6551a338
visualstudio <visualstudio@122-PC11>
2019-03-08 21:38:37 -0500

    Lab 16

---
+++ C0730065-C0730323/DelegateExercises.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C0730065_C0730323
+{
+    internal class DelegateExercises
+    {
+        public DelegateExercises()
+        {
+        }
+
+        internal void Method3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- C0730065-C0730323/Program.cs
+++ C0730065-C0730323/Program.cs
@@ -9,40 +9,19 @@ namespace C0730065_C0730323
     class program
     {
 
-    
-    // Parminder Kaur Bhangu (C0730065)
-    //Ravneet Kaur (C0730323)
-    public static void Main(string[] args)
-    {
-            DelegateExercises de = new DelegateExercises();
-            de.Method3();
+
+        // Parminder Kaur Bhangu (C0730065)
+        //Ravneet Kaur (C0730323)
+        public static void Main(string[] args)
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            delegateExercises.Method3();
+
             Console.ReadLine();
         }
     }
-    delegate int MyDelegate(out int i);
-    public class DelegateExercises
-    {
-        int Method1(out int i)
-        {
-
-            i = 100;
-            Console.WriteLine("Method 1 " + i);
-            return 0;
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
-        }
-
-    }
-
 }
-
-
 
 
 
